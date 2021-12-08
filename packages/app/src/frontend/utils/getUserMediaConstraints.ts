97af580884744aaf6bd642a7c9b1e9df773c1a5f
Florian Biewald <florianbiewald@googlemail.com>
2021-12-08 14:46:37 +0100

    #41 app: media constraints

--- packages/app/src/frontend/utils/getUserMediaConstraints.ts
+++ packages/app/src/frontend/utils/getUserMediaConstraints.ts
@@ -1,6 +1,10 @@
 export const getUserMediaConstraints = (): MediaStreamConstraints => {
   return {
-    video: { width: { exact: 1280 }, height: { exact: 720 } },
+    video: {
+      width: { ideal: 1280 },
+      height: { ideal: 720 },
+      aspectRatio: 16 / 9,
+    },
     // audio: true,
   }
 }
